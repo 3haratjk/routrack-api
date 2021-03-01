125d5c19404a1f9120b2b043cca204480d95b41c
SpontaBharat <spontabharat@gmail.com>
2021-03-01 11:30:37 +0530

    changed port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const trackRoutes = require('./routes/trackRoutes');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT && 3000;
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.rtnra.gcp.mongodb.net/trackme?retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri, {
