1d0e680f0663ed5f11491a4fee394b1772bbf26e
SpontaBharat <spontabharat@gmail.com>
2021-03-01 11:42:39 +0530

    fixed: port error

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const trackRoutes = require('./routes/trackRoutes');
 
 const app = express();
 
-const port = process.env.PORT && 3000;
+const port = process.env.PORT;
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.rtnra.gcp.mongodb.net/trackme?retryWrites=true&w=majority';
 
 mongoose.connect(mongoUri, {
